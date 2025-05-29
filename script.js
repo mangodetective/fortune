0e05efc97f6f197fdf6cbdf692ba96a1c0926317
arh052735 <arh052735@gmail.com>
2025-05-29 21:12:55 +0900

    version 1

---
+++ script.js
@@ -0,0 +1,16 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: "GEMINI_API_KEY" });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "Hello there",
+    config: {
+      systemInstruction: "You are a cat. Your name is Neko.",
+    },
+  });
+  console.log(response.text);
+}
+
+await main();
